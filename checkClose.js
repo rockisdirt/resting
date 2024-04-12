226cfba078cb4f0f09b3cdf49bd06013d3c9bc01
rockisdirt <124087276+rockisdirt@users.noreply.github.com>
2024-04-12 13:24:35 -0700

    Update checkClose.js

--- checkClose.js
+++ checkClose.js
@@ -1,15 +1,4 @@
-
-function checkClose() {
-    if (ok) {
-        return "Have you saved your information?";
-    }
-}
-
-var ok = true; // this is reset everytime a new page is loaded
-
-function okToLeave() { // called if leaving the page is needed
-    ok = false;
-}
-
-window.onbeforeunload = checkClose;
-window.onunload = checkClose;
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    e.returnValue = '';
+});
